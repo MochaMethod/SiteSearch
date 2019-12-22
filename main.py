ba6f4db244d5fee67cdf1e330afe1695c1fb2556
mochamethod <mochamethod@gmail.com>
2019-12-22 14:18:57 -0700

    Cleaned up the UI a bit. Hooked in functionality to the UI; binded the Enter key to the Submit button. TODO: Show output in TextBox.

--- main.py
+++ main.py
@@ -4,44 +4,8 @@ from bs4 import BeautifulSoup
 import urllib
 from tkinter import *
 
-def createUI(root):
-    siteGrid = Frame(root, width=450, height=50, padx=5, pady=5)
-    queryGrid = Frame(root, width=450, height=50, padx=5, pady=5)
-    amountGrid = Frame(root, width=450, height=50, padx=5, pady=5)
-    outputGrid = Frame(root, width=450, height=50, padx=10, pady=10)
-
-    root.grid_rowconfigure(1, weight=1)
-    root.grid_columnconfigure(0, weight=1)
-
-    siteLabel = Label(siteGrid, text="Site: ")
-    siteEntry = Entry(siteGrid, width=100)
-
-    queryLabel = Label(queryGrid, text="Search: ")
-    queryEntry = Entry(queryGrid, width=100)
-
-    resultsLabel = Label(amountGrid, text="Results Amount: ")
-    resultsEntry = Entry(amountGrid, width=5)
-
-    outputLabel = Label(outputGrid, text="Output: ")
-    outputText = Text(outputGrid)
-    outputText.configure(state="disabled", bg="#e9e9e9")
-
-    siteLabel.grid(row=0, column=0, sticky=W)
-    siteEntry.grid(row=0, column=1)
-
-    queryLabel.grid(row=0, column=0, sticky=W)
-    queryEntry.grid(row=0, column=1)
-
-    resultsLabel.grid(row=0, column=0, sticky=W)
-    resultsEntry.grid(row=0, column=1)
-
-    outputLabel.grid(row=0, column=0, sticky=W)
-    outputText.grid(row=1, column=0)
-
-    siteGrid.grid(row=0, column=0, sticky=NW)
-    queryGrid.grid(row=1, column=0, sticky=NW)
-    amountGrid.grid(row=2, column=0, sticky=NW)
-    outputGrid.grid(row=3, column=0, sticky=NW)
+def output(root, outputWidget, message):
+    outputWidget.insert("end", message)
 
 def googleQuery(query, resultsAmt, site=None):
     resultsDict = {}
@@ -82,16 +46,80 @@ def googleQuery(query, resultsAmt, site=None):
 
     return resultsDict
 
+def handleSubmit(root, outputWidget, queryEntry, resultsEntry, siteEntry):
+    query = queryEntry.get()
+    amount = int(resultsEntry.get())
+    site = siteEntry.get()
+
+    resultsDict = googleQuery(query, amount, site)
+    for k, v in resultsDict.items():
+        message = (f"\nTitle: {k}\nURL: {v}\n")
+        print(message)
+        output(root, outputWidget, message)
+    
+
+def createUI(root):
+    siteGrid = Frame(root, width=450, height=50, padx=5, pady=5)
+    queryGrid = Frame(root, width=450, height=50, padx=5, pady=5)
+    amountGrid = Frame(root, width=450, height=50, padx=5, pady=5)
+    submitGrid = Frame(root, width=450, height=50, padx=5, pady=5)
+    outputGrid = Frame(root, width=450, height=50, padx=10, pady=10)
+
+    root.grid_rowconfigure(1, weight=1)
+    root.grid_columnconfigure(0, weight=1)
+    
+    queryLabel = Label(queryGrid, text="* Search: ")
+    queryEntry = Entry(queryGrid, width=110)
+
+    resultsLabel = Label(amountGrid, text="* Desired amount of results: ")
+    resultsEntry = Entry(amountGrid, width=5)
+
+    siteLabel = Label(siteGrid, text="Site-specific search: ")
+    siteEntry = Entry(siteGrid, width=100)
+
+    outputLabel = Label(outputGrid, text="Output: ")
+    outputText = Text(outputGrid)
+    outputText.configure(state="disabled", bg="#e9e9e9")
+    
+    resultsSubmit = Button(submitGrid, text="Search", command=lambda: handleSubmit(root, outputText, queryEntry, resultsEntry, siteEntry))
+
+    queryLabel.grid(row=0, column=0, sticky=W)
+    queryEntry.grid(row=0, column=1)
+
+    resultsLabel.grid(row=0, column=0, sticky=W)
+    resultsEntry.grid(row=0, column=1)
+
+    resultsSubmit.grid(row=0, column=2, padx=25)
+
+    siteLabel.grid(row=0, column=0, sticky=W)
+    siteEntry.grid(row=0, column=1)
+
+    outputLabel.grid(row=0, column=0, sticky=W)
+    outputText.grid(row=1, column=0)
+
+    queryGrid.grid(row=0, column=0, sticky=NW)
+    amountGrid.grid(row=1, column=0, sticky=NW)
+    siteGrid.grid(row=2, column=0, sticky=NW)
+    submitGrid.grid(row=0, column=1, sticky=NE)
+    outputGrid.grid(row=3, column=0, sticky=NW)
+
+    root.bind(
+        "<Return>", 
+        lambda root=root, outputText=outputText, queryEntry=queryEntry, amountEntry=resultsEntry, siteEntry=siteEntry: handleSubmit(
+            root, outputText, queryEntry, resultsEntry, siteEntry
+            )
+        )
+
 def main():
     root = Tk()
     createUI(root)
     root.mainloop()
 
-    #query = "C++ arrays"
-    #resultsDict = googleQuery(query, 1, "stackoverflow.com")
+    query = "C++ arrays"
+    resultsDict = googleQuery(query, 1, "stackoverflow.com")
 
-    #for k, v in resultsDict.items():
-    #    print(f"\nTitle: {k}\nURL: {v}\n")
+    for k, v in resultsDict.items():
+        print(f"\nTitle: {k}\nURL: {v}\n")
 
     return 0
 
